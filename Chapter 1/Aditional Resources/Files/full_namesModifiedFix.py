4471a68ab99222880a8d5e87f26f9bba7def789c
nflorez228 <35770924+nflorez228@users.noreply.github.com>
2020-04-14 11:43:11 -0500

    Create full_namesModifiedFix.py

---
+++ Chapter 1/Aditional Resources/Files/full_namesModifiedFix.py
@@ -0,0 +1,9 @@
+def get_full_nameFix(first, last, middle=''):
+ """Return a full name."""
+ if middle:
+ full_name = "{0} {1} {2}".format(first,
+ middle, last)
+ else:
+ full_name = "{0} {1}".format(first,
+ last)
+ return full_name.title()
